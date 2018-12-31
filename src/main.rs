cc23ef2dab62aafb4478785461ed26a3100c3fd0
Stephen Swensen <stephen.swensen@gmail.com>
2018-12-31 11:43:54 -0500

    draw a rect texture

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,40 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
 use std::thread::sleep;
+use sdl2::rect::Rect;
+use sdl2::render::{Canvas, Texture, TextureCreator};
+
+const TEXTURE_SIZE: u32 = 32;
 
 pub fn main() {
     let sdl_context = sdl2::init().expect("SDL init failed");
     let video_subsystem = sdl_context.video().expect("Couldn't get SDL video subsystem");
 
-    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
+    let window = video_subsystem
+        .window("rust-sdl2 demo: Video", 800, 600)
         .position_centered()
-        .opengl()
+        //.opengl()
         .build()
         .expect("Failed to create window");
 
-    let mut canvas = window.into_canvas().build().expect("Failed to convert window into canvas");
+    let mut canvas = window
+        .into_canvas()
+        .target_texture()
+        .present_vsync()
+        .build()
+        .expect("Failed to convert window into canvas");
 
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
-    canvas.clear();
-    canvas.present();
+    let texture_creator: TextureCreator<_> = canvas.texture_creator();
+    let mut square_texture: Texture = texture_creator
+        .create_texture_target(None, TEXTURE_SIZE, TEXTURE_SIZE)
+        .expect("Failed to create a texture");
+    
+    canvas.with_texture_canvas(&mut square_texture, |texture| {
+        texture.set_draw_color(Color::RGB(0, 255, 0));
+        texture.clear();
+    }).expect("Failed to color a texture");
+
     let mut event_pump = sdl_context.event_pump().expect("Failed to get SDL event pump");
-
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -40,6 +56,16 @@ pub fn main() {
                 _ => ()
             }
         }
+
+        //canvas.set_draw_color(Color::RGB(255, 0, 0));
+        canvas.clear();
+        canvas.copy(
+            &square_texture, 
+            None, 
+            Rect::new(0, 0, TEXTURE_SIZE, TEXTURE_SIZE)
+        ).expect("Couldn't copy texture into window");
+
+        canvas.present();
         sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
